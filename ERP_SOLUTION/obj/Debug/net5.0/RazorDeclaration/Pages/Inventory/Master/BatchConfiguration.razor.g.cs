91af6009e7d1ee6d84b6cf1eb809f7bdcc5d7eca
kawserkibria <kawser.2000.dhaka@gmail.com>
2024-05-28 14:22:03 +0600

    ok

--- ERP_SOLUTION/obj/Debug/net5.0/RazorDeclaration/Pages/Inventory/Master/BatchConfiguration.razor.g.cs
+++ ERP_SOLUTION/obj/Debug/net5.0/RazorDeclaration/Pages/Inventory/Master/BatchConfiguration.razor.g.cs
@@ -6,90 +6,87 @@
 
 namespace ERP_SOLUTION.Pages.Inventory.Master
 {
-    #line default
-    using global::System;
-    using global::System.Collections.Generic;
-    using global::System.Linq;
-    using global::System.Threading.Tasks;
-    using global::Microsoft.AspNetCore.Components;
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using System.Net.Http
-
-#nullable disable
-    ;
-#nullable restore
-#line 2 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Authorization
-
-#nullable disable
-    ;
-#nullable restore
-#line 3 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Components.Authorization
-
-#nullable disable
-    ;
-#nullable restore
-#line 4 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Components.Forms
-
-#nullable disable
-    ;
-#nullable restore
-#line 5 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Components.Routing
-
-#nullable disable
-    ;
-#nullable restore
-#line 6 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web
-
-#nullable disable
-    ;
-#nullable restore
-#line 7 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web.Virtualization
-
-#nullable disable
-    ;
-#nullable restore
-#line 8 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using Microsoft.JSInterop
-
-#nullable disable
-    ;
-#nullable restore
-#line 9 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using ERP_SOLUTION
-
-#nullable disable
-    ;
-#nullable restore
-#line 10 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
-using ERP_SOLUTION.Shared
+#line 1 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
-    ;
-    [global::Microsoft.AspNetCore.Components.RouteAttribute(
-    // language=Route,Component
 #nullable restore
-#line 2 "C:\Users\Administrator\Desktop\Kawsar\Kawser kibria\ERP_SOLUTION\ERP_SOLUTION\Pages\Inventory\Master\BatchConfiguration.razor"
-      "/BatchConfiguration"
+#line 2 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
-    )]
-    #nullable restore
-    public partial class BatchConfiguration : global::Microsoft.AspNetCore.Components.ComponentBase
-    #nullable disable
+#nullable restore
+#line 3 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Components.Authorization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Components.Forms;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Components.Routing;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 7 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 8 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using Microsoft.JSInterop;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 9 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using ERP_SOLUTION;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 10 "G:\ASAD VAI\ERP_SOLUTION\ERP_SOLUTION\_Imports.razor"
+using ERP_SOLUTION.Shared;
+
+#line default
+#line hidden
+#nullable disable
+    [Microsoft.AspNetCore.Components.RouteAttribute("/BatchConfiguration")]
+    public partial class BatchConfiguration : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
-        protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
